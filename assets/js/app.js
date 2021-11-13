80c70f4f423603387a890f2ab9f37f09da2cc250
Christian Fei <crifei93@gmail.com>
2021-11-13 13:39:20 +0100

    orderbook visualization

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,6 +49,7 @@ const MAX_VISIBLE_MATCHES = 20
 const $matches = document.querySelector('.matches')
 const $level2s = document.querySelector('.level2')
 const $orderbook = document.querySelector('.orderbook')
+const svg = d3.select('.orderbook-graph svg')
 
 let matchesChannel = socket.channel("data:matches", {})
 matchesChannel.join()
@@ -65,20 +66,35 @@ orderbookChannel.join()
 
 orderbookChannel.on("data", (message) => {
   const { data: orderbook } = message
+
   const spread = orderbook.asks[0][0] - orderbook.bids[0][0]
   const asks = orderbook.asks.slice(0, 10).reverse()
   const bids = orderbook.bids.slice(0, 10)
+
+  // {type, price, total}
+  let minAskPrice = Math.min(...orderbook.asks.map(a => +a[0]))
+  let maxBidPrice = Math.max(...orderbook.bids.map(a => +a[0]))
+  console.log({
+    minAskPrice
+    , maxBidPrice
+  })
+  let sumAsk = 0
+  let sumBid = 0
+  const orderbookGraphData = orderbook.asks.filter(a => +a[0] < minAskPrice + 1000) //.slice(0, 50)
+    .map(a => ({ type: 'ask', price: +a[0], total: sumAsk += +a[1] }))
+    .concat(
+      orderbook.bids.filter(a => +a[0] > maxBidPrice - 1000) //.slice(0, 50)
+        .map(a => ({ type: 'bid', price: +a[0], total: sumBid += +a[1] }))
+    )
+
+  draw(orderbookGraphData, svg)
 
   const maxSize = Math.max(...asks.map(o => o[1]).concat(bids.map(o => o[1])))
   const calcPercentage = size => size / maxSize * 100
 
   $orderbook.innerHTML = `
     ${asks.map(o => `<div class="orderbook-item orderbook-buy">${o[1]} ${o[0]}<div class="bg-size" style="width: ${calcPercentage(o[1]) / 10}%"></div></div>`).join('')}
-    <br>
-    <br>
     <div class="orderbook-spread">Spread ${spread.toFixed(2)}</div>
-    <br>
-    <br>
     ${bids.map(o => `<div class="orderbook-item orderbook-sell">${o[1]} ${o[0]}<div class="bg-size" style="width: ${calcPercentage(o[1]) / 10}%"></div></div>`).join('')}
   `
 })
@@ -113,3 +129,83 @@ matchesChannel.on("data", (message) => {
     last.parentNode.removeChild(last)
   }
 })
+
+function draw(unsortedData, target, d3 = window.d3) {
+  d3.selectAll("svg > *").remove();
+
+  const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+  const width = target.node().clientWidth - margin.left - margin.right;
+  const height = target.node().clientHeight - margin.top - margin.bottom;
+  const x = d3.scaleLinear().range([0, width]);
+  const y = d3.scaleLinear().range([height, 0]);
+
+  const g = target.append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
+
+  const data = unsortedData.sort((a, b) => (a.price > b.price ? 1 : -1));
+
+  x.domain([
+    d3.min(data, d => d.price),
+    d3.max(data, d => d.price) + 1,
+  ]);
+  y.domain([0, d3.max(data, d => d.total)]);
+
+  g.append('g')
+    .attr('class', 'axis axis--x')
+    .attr('transform', `translate(0,${height})`)
+    .call(d3.axisBottom(x));
+
+  g.append('g')
+    .attr('class', 'axis axis--y')
+    .call(d3.axisLeft(y));
+
+  // // Define the div for the tooltip
+  // const tooltip = d3.select('body').append('div')
+  //   .attr('class', 'orderbook-visualisation-tooltip')
+  //   .style('position', 'absolute')
+  //   .style('top', `${target.node().parentNode.offsetTop}px`)
+  //   .style('left', `${(target.node().parentNode.offsetLeft + margin.left + (width / 2)) - 100}px`)
+  //   .style('width', '200px')
+  //   .style('opacity', 0)
+  //   .html('');
+
+  g.selectAll('.bar')
+    .data(data)
+    .enter().append('rect')
+    .attr('class', d => `bar ${d.type}`)
+    .attr('x', d => x(d.price))
+    .attr('y', d => y(d.total))
+    .attr('width', (d, i) => {
+      // is there a next element and do they have the same type:
+      // fill until the next order
+      if (data[i + 1] && data[i + 1].type === d.type) {
+        return x(data[i + 1].price) - x(d.price);
+        // is there a next element and they don't have the same type:
+        // market price valley
+      } else if (data[i + 1]) {
+        return (x.range()[1] - x.range()[0]) / data.length;
+      }
+      // this is the last element: fill until the end of the graph
+      return x.range()[1] - x(d.price);
+    })
+    .attr('height', d => height - y(d.total))
+  // .on('mouseover', (d) => {
+  //   tooltip.transition()
+  //     .duration(500)
+  //     .style('opacity', 1);
+
+  //   let html = '<table>';
+
+  //   Object.keys(d).forEach((key) => {
+  //     html += `<tr><td><b>${key}</b></td><td>${d[key]}</td></tr>`;
+  //   });
+
+  //   html += '</table>';
+
+  //   tooltip.html(html);
+  // })
+  // .on('mouseout', () =>
+  //   tooltip.transition().duration(500).style('opacity', 0),
+  // );
+};
+
