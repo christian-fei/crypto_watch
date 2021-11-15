3af1a0c2ffb2b2190f7fe71bbd703f98d70b8aa4
Christian Fei <crifei93@gmail.com>
2021-11-15 19:08:54 +0100

    cache tests orderbook

--- test/crypto_watch/cache_test.exs
+++ test/crypto_watch/cache_test.exs
@@ -7,7 +7,7 @@ defmodule CryptoWatch.CacheTest do
            |> Process.alive?()
   end
 
-  test "caches match" do
+  test "add match and get cached matches" do
     match = %{
       side: "buy",
       size: 0.024218,
@@ -18,5 +18,16 @@ defmodule CryptoWatch.CacheTest do
     {:ok, matches} = GenServer.call(CryptoWatch.Cache, {:get_matches, "BTC-EUR"})
 
     assert matches == [match]
+  end
+
+  test "update orderbook and get cached orderbook" do
+    orderbook = %{
+      asks: [],
+      bids: [],
+      time: ""
+    }
+
+    GenServer.cast(CryptoWatch.Cache, {:update_order_book, "BTC-EUR", orderbook})
+    {:ok, ^orderbook} = GenServer.call(CryptoWatch.Cache, {:get_order_book, "BTC-EUR"})
   end
 end
