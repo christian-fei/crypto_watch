882c260e5000572add02415409c37603b93a9bc1
Christian Fei <crifei93@gmail.com>
2021-11-15 19:09:18 +0100

    @impl GenServer

--- lib/crypto_watch/cache.ex
+++ lib/crypto_watch/cache.ex
@@ -5,7 +5,7 @@ defmodule CryptoWatch.Cache do
     GenServer.start_link(__MODULE__, :ok, opts)
   end
 
-  @impl true
+  @impl GenServer
   def init(_) do
     Process.register(self(), __MODULE__)
 
@@ -16,29 +16,29 @@ defmodule CryptoWatch.Cache do
      }}
   end
 
-  @impl true
+  @impl GenServer
   def handle_call({:get_order_book, name}, _from, state) do
     {:reply, Map.fetch(state.order_books, name), state}
   end
 
-  @impl true
+  @impl GenServer
   def handle_call({:get_matches, name}, _from, state) do
     {:reply, Map.fetch(state.matches, name), state}
   end
 
-  @impl true
+  @impl GenServer
   def handle_cast({:update_order_book, name, order_book}, state) do
     state = Map.merge(state, %{order_books: Map.put(%{}, name, order_book)})
     {:noreply, state}
   end
 
-  @impl true
+  @impl GenServer
   def handle_cast({:update_matches, name, matches}, state) do
     state = Map.merge(state, %{matches: Map.put(%{}, name, matches)})
     {:noreply, state}
   end
 
-  @impl true
+  @impl GenServer
   def handle_cast({:add_match, name, match}, state) do
     updated_matches =
       Map.put(%{}, name, (Map.get(state.matches, name, []) ++ [match]) |> Enum.take(500))
